6bb424ce066947f24abaf9b552a98e9f4f850305
yni <162464115+Johnniel3@users.noreply.github.com>
2025-07-16 14:37:25 -0300

    segundo commit

--- config/dataBase.js
+++ config/dataBase.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 const db = new Sequelize(process.env.NAME_DB,process.env.USER_DB, process.env.PASS_DB, {
     host: process.env.HOST_DB,
-    port: 3306,
+    port: 61002,
     dialect: "mysql",
     define: {
         timestamps: true
